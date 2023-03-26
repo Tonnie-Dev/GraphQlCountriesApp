263e0988355a60400917af754b6dd25688828487
Tonnie <vontonnie@gmail.com>
2023-03-26 20:34:40 +0300

    Add ApolloCountryClient class inside the data package

--- app/src/main/java/com/plcoding/graphqlcountriesapp/data/ApolloCountryClient.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/data/ApolloCountryClient.kt
@@ -1,17 +1,26 @@
 package com.plcoding.graphqlcountriesapp.data
 
 import com.apollographql.apollo3.ApolloClient
+import com.plcoding.CountriesQuery
+import com.plcoding.CountryQuery
+import com.plcoding.graphqlcountriesapp.data.mapper.toDetailedCountry
+import com.plcoding.graphqlcountriesapp.data.mapper.toSimpleCountry
 import com.plcoding.graphqlcountriesapp.domain.CountryClient
 import com.plcoding.graphqlcountriesapp.domain.DetailedCountry
 import com.plcoding.graphqlcountriesapp.domain.SimpleCountry
 
-class ApolloCountryClient(private val apolloClient: ApolloClient):CountryClient {
+class ApolloCountryClient(private val apolloClient: ApolloClient) : CountryClient {
 
     override suspend fun getCountries(): List<SimpleCountry> {
-        TODO("Not yet implemented")
+
+        return apolloClient.query(CountriesQuery())
+                .execute().data
+                ?.countries
+                ?.map { it.toSimpleCountry() } ?: emptyList()
     }
 
     override suspend fun getCountry(code: String): DetailedCountry? {
-        TODO("Not yet implemented")
+        return apolloClient.query(CountryQuery(code))
+                .execute().data?.country?.toDetailedCountry()
     }
 }
