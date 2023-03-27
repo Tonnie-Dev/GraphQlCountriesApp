4a60283ad35de2b958e041db4fc53f80ef1773fc
Tonnie <vontonnie@gmail.com>
2023-03-27 07:20:33 +0300

    Add init block inside CountriesViewModel

--- app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesState.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesState.kt
@@ -1,10 +1,11 @@
 package com.plcoding.graphqlcountriesapp.presentation
 
 import com.plcoding.graphqlcountriesapp.domain.model.DetailedCountry
+import com.plcoding.graphqlcountriesapp.domain.model.SimpleCountry
 import java.util.*
 
 data class CountriesState(
-    val countries: List<Scanner> = emptyList(),
+    val countries: List<SimpleCountry> = emptyList(),
     val isLoading: Boolean = false,
     val selectedCountry: DetailedCountry? = null
 )
--- app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesViewModel.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesViewModel.kt
@@ -1,16 +1,33 @@
 package com.plcoding.graphqlcountriesapp.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.plcoding.graphqlcountriesapp.domain.use_cases.GetCountriesUseCase
 import com.plcoding.graphqlcountriesapp.domain.use_cases.GetCountryUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class CountriesViewModel(
     private val getCountryUseCase: GetCountryUseCase,
     private val getCountriesUseCase: GetCountriesUseCase
-):ViewModel(){
+) : ViewModel() {
 
-private val _state = MutableStateFlow(CountriesState())
+    private val _state = MutableStateFlow(CountriesState())
     val state = _state.asStateFlow()
+
+
+    init {
+
+        viewModelScope.launch {
+
+            _state.update { it.copy(isLoading = true) }
+
+            /*This will suspend for amount of time it takes to finish the request
+            * and after that isLoading will be set to false*/
+
+            _state.update { it.copy(countries = getCountriesUseCase.execute(), isLoading = false) }
+        }
+    }
 }
