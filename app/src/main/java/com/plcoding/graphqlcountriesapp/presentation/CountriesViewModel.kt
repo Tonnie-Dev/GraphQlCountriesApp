093e4515f677624f6e7f9601301284b54d85b792
Tonnie <vontonnie@gmail.com>
2023-03-27 07:14:37 +0300

    Add CountriesViewModel class

--- app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesViewModel.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesViewModel.kt
@@ -3,11 +3,14 @@ package com.plcoding.graphqlcountriesapp.presentation
 import androidx.lifecycle.ViewModel
 import com.plcoding.graphqlcountriesapp.domain.use_cases.GetCountriesUseCase
 import com.plcoding.graphqlcountriesapp.domain.use_cases.GetCountryUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class CountriesViewModel(
     private val getCountryUseCase: GetCountryUseCase,
     private val getCountriesUseCase: GetCountriesUseCase
 ):ViewModel(){
 
-
+private val _state = MutableStateFlow(CountriesState())
+    val state = _state.asStateFlow()
 }
