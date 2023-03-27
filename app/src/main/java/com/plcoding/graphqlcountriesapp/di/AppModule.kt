208ba6f06dead59e7eef8557deec3a22206c1cca
Tonnie <vontonnie@gmail.com>
2023-03-27 08:33:06 +0300

    Add provideApolloClient() di function

--- app/src/main/java/com/plcoding/graphqlcountriesapp/di/AppModule.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/di/AppModule.kt
@@ -1,12 +1,19 @@
 package com.plcoding.graphqlcountriesapp.di
 
+import com.apollographql.apollo3.ApolloClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    
+    @Provides
+    @Singleton
+    fun provideApolloClient(): ApolloClient = ApolloClient.Builder()
+            .serverUrl("https://countries.trevorblades.com/graphql")
+            .build()
 }
