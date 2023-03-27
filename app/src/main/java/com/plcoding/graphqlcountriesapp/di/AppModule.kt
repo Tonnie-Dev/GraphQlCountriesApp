4884598661cd9e650c2583dd183e8f0d8e74e2ff
Tonnie <vontonnie@gmail.com>
2023-03-27 08:41:46 +0300

    Add provideGetCountryUseCase() di function

--- app/src/main/java/com/plcoding/graphqlcountriesapp/di/AppModule.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.plcoding.graphqlcountriesapp.di
 
 import com.apollographql.apollo3.ApolloClient
+import com.plcoding.graphqlcountriesapp.data.ApolloCountryClient
+import com.plcoding.graphqlcountriesapp.domain.CountryClient
+import com.plcoding.graphqlcountriesapp.domain.use_cases.GetCountryUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,4 +19,14 @@ object AppModule {
     fun provideApolloClient(): ApolloClient = ApolloClient.Builder()
             .serverUrl("https://countries.trevorblades.com/graphql")
             .build()
+
+    @Provides
+    @Singleton
+    fun provideCountryClient(apolloClient: ApolloClient): CountryClient =
+        ApolloCountryClient(apolloClient)
+
+
+    @Provides
+    @Singleton
+    fun provideGetCountryUseCase(countryClient: CountryClient) = GetCountryUseCase(countryClient)
 }
