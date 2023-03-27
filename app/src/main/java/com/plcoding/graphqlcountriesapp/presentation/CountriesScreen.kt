cec18e770285608da4e92be94f943a4b2c8c2a2c
Tonnie <vontonnie@gmail.com>
2023-03-27 21:05:22 +0300

    Add CountryDialso composable

--- app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesScreen.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/presentation/CountriesScreen.kt
@@ -9,11 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.plcoding.graphqlcountriesapp.domain.model.DetailedCountry
 import com.plcoding.graphqlcountriesapp.domain.model.SimpleCountry
 
 @Composable
@@ -23,7 +26,7 @@ fun CountriesScreen(
     onDismissCountryDialog: () -> Unit
 ) {
 
-     val state by viewModel.state.collectAsState()
+    val state by viewModel.state.collectAsState()
     Box(modifier = Modifier.fillMaxSize()) {
 
         if (state.isLoading) {
@@ -59,6 +62,41 @@ fun CountryItem(country: SimpleCountry, modifier: Modifier = Modifier) {
             Text(text = country.name, fontSize = 24.sp)
             Spacer(modifier = Modifier.width(16.dp))
             Text(text = country.capital)
+        }
+    }
+}
+
+@Composable
+private fun CountryDialog(
+    country: DetailedCountry,
+    onDismissCountryDialog: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    val joinedLanguages = remember(country.languages) { country.languages.joinToString() }
+    Dialog(onDismissRequest = onDismissCountryDialog) {
+
+        Column(modifier = modifier) {
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+
+                Text(text = country.emoji, fontSize = 30.sp)
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(text = country.name, fontSize = 24.sp)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Continent: ${country.continent}")
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = "Currency: ${country.currency}")
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = "Capital: ${country.capital}")
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = "Languages(s): $joinedLanguages")
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
